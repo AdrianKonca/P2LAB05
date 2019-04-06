31e62c00405e6aae6bedbdd7e438fc76728ae8bd
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2019-04-06 20:35:23 +0200

    Adds base classes

---
+++ P2LAB05/Item.h
@@ -0,0 +1,10 @@
+#pragma once
+class Item
+{
+	std::string name;
+	int value;
+public:
+	Item();
+	~Item();
+};
+
---
+++ P2LAB05/Room.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Item.h"
+
+class Room
+{
+	std::string description;
+	int x;
+	int y;
+	Room *neighborN;
+	Room *neighborW;
+	Room *neighborE;
+	Room *neighborS;
+	Item *item;
+public:
+	std::string getDescription();
+	Room *getNeighborN();
+	Room *getNeighborS();
+	Room *getNeighborE();
+	Room *getNeighborW();
+	bool hasItem();
+	Item *getItem();
+	Room();
+	~Room();
+};
+
